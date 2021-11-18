556a47f831adecc21f35096a21fc25324991876c
annekegilbert <93998818+annekegilbert@users.noreply.github.com>
2021-11-18 10:13:41 -0500

    Add files via upload

---
+++ plotData.m
@@ -0,0 +1,15 @@
+% vizualize the potential field for all 15 models 
+% model numbers are based on tabel 1
+% each model .txt file contains x,y,z cooordinates and corresponding potential (V)
+
+% select model number
+model_number = 1;
+
+% load potential field
+data = load(['model_' num2str(model_number) '.txt']);
+
+% plot potential field
+figure; scatter3(data(:,1),data(:,2),data(:,3),[],data(:,4),'Marker','.')
+    axis equal
+    colormap(flipud(jet))
+    view([-125 20])
